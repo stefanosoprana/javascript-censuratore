485e8e0b09becb9a9a23b792350a181d3d896a8e
stefanosoprana <stefano.soprana@icloud.com>
2018-11-21 17:56:56 +0100

    fix javascript - censuratore

--- main.js
+++ main.js
@@ -6,9 +6,15 @@
 // parole censurate su il numero di parole totali
 
 var frase = 'La Luna è un satellite naturale, l\' unico della Terra. Il suo nome proprio viene talvolta utilizzato, per antonomasia e con l\' iniziale minuscola («una luna»), come sinonimo di satellite anche per i corpi celesti che orbitano attorno ad altri pianeti.';
+
 console.log(frase);
 
-var wordCensored = /luna|pianeti|terra|nome|satellite/gi;
+var Badwords = ['luna', 'pianeti', 'terra', 'nome', 'satellite'];
 
-  var replaced = frase.replace(wordCensored, "xxx");
-  console.log(replaced);
+for (var i = 0; i < Badwords.length; i++) {
+
+  var frase = frase.replace(Badwords[i],"xxx");
+  
+}
+
+console.log(frase);
